4cb3a39d530200575078898d88e6a4c56632c0c9
rosanaarj <rosanamaria0981@gmail.com>
2025-03-21 14:26:29 +0000

    questao01

---
+++ atividade01-TiposDados/questao01.js
@@ -0,0 +1,13 @@
+let t1 = 5;
+let t2 = 4;
+let t3 = 3;
+let t4 = 2;
+let t5 = 1;
+let txt1 = "aprendi";
+let txt2 = "JavaScript";
+
+console.log(n1+","+ n2+"," +n3+","+ n4+","+ n5+ "..."+ txt1 + txt2);
+
+
+
+
