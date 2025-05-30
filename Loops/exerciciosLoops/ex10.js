f9d50184f35a2106038e379832a7e2c481d45dfd
rosanaarj <rosanamaria0981@gmail.com>
2025-05-30 14:19:37 +0000

    salvando!

---
+++ Loops/exerciciosLoops/ex10.js
@@ -0,0 +1,10 @@
+/**
+ * 10) Faça um programa que exiba o produto dos números múltiplos de 7 que estão entre 100 e 1000.
+ */
+
+let produto = 120;
+for (let i = 100; i <= 1000; i++) {
+    if (i % 7 === 0) {
+        console.log(i);  
+    }
+}
